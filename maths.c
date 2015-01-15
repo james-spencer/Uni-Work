b937404b0d179c52c9d890411ddb07d6c7a61c87
James Spencer <p13226985@myemail.dmu.ac.uk>
2015-01-15 16:45:57 +0000

    Added addition feature

---
+++ maths.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+        int x, y, z;
+        
+        x = 10;
+        y = 5;
+        z = 0;
+        
+        z = x + y;
+        
+        printf("%i\n", z);
+        
+        return 0;
+}
