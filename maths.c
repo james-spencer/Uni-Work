68a8c74d3cbc5b73b778654f4318e48727d1b018
James Spencer <p13226985@myemail.dmu.ac.uk>
2015-01-22 16:22:26 +0000

    now including forgotten.txt

--- maths.c
+++ maths.c
@@ -2,24 +2,31 @@
 
 int main()
 {
-        int x, y, a, b, c, d;
+        int x, y, a, b, c, d, in1, in2, in3, in4, in5, avg, count;
         
-        x = 10;
-        y = 5;
-        a = 0;
-        b = 0;
-        c = 0;
-        d = 0;
+        printf("Please enter a min of 1 no. and max of 5 no. to find the average of\n");
+        scanf("%i %i %i %i %i", &in1, &in2, &in3, &in4, &in5);
         
-        a = x + y;
-        b = x - y;
-        c = x * y;
-        d = x / y;
+        avg = (in1 + in2 + in3 + in4 + in5) / 5;
         
-        printf("%i\n", a);
-        printf("%i\n", b);
-        printf("%i\n", c);
-        printf("%i\n", d);
+        //x = 10;
+        //y = 5;
+        //a = 0;
+        //b = 0;
+        //c = 0;
+        //d = 0;
+        //
+        //a = x + y;
+        //b = x - y;
+        //c = x * y;
+        //d = x / y;
+        //
+        //printf("%i\n", a);
+        //printf("%i\n", b);
+        //printf("%i\n", c);
+        //printf("%i\n", d);
+        
+        printf("Avg = %i\n", avg);
         
         return 0;
 }
